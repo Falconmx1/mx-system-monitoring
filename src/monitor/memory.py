2aaf5891af1365cd109a30a2362700f177bb6070
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-07-02 04:41:59 -0600

    Create memory.py

---
+++ src/monitor/memory.py
@@ -0,0 +1,15 @@
+import psutil
+
+def get_memory_info():
+    """Obtiene información de la memoria"""
+    mem = psutil.virtual_memory()
+    swap = psutil.swap_memory()
+    return {
+        'total': mem.total,
+        'available': mem.available,
+        'used': mem.used,
+        'percent': mem.percent,
+        'swap_total': swap.total,
+        'swap_used': swap.used,
+        'swap_percent': swap.percent
+    }
